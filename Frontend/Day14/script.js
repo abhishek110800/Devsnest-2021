2d3dcfbae8f20b995a5c8ad4612487d8d44a41c1
Abhishek Singh <s.abhi474singh@gmail.com>
2021-07-01 21:54:41 +0530

    Add files via upload

---
+++ Frontend/Day14/script.js
@@ -0,0 +1,18 @@
+function onClick() {
+    console.log("Mouse is clicked");
+  }
+  function ondblClick() {
+    console.log("Double Clicked");
+  }
+  function drag() {
+    console.log("Dragged");
+  }
+  function mouseDw() {
+    console.log("Mouse Down");
+  }
+  function mseMove() {
+    console.log("Mouse Moved");
+  }
+  function mseOver() {
+    console.log("Mouse Over");
+  }
