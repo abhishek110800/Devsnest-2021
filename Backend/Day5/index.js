0ddf68e42c907f1c56a3557f755ed625cc90afde
Abhishek Singh <s.abhi474singh@gmail.com>
2021-09-16 22:32:49 +0530

    Delete Backend/Day5 directory

--- Backend/Day5/index.js
+++
@@ -1,7 +0,0 @@
-var fs=require("fs");
-fs.writeFileSync("day5/tha5.txt","Hello");
-fs.readFileSync("day5/tha5.txt","utf-8");
-fs.appendFileSync("day5/tha5.txt"," World!");
-fs.unlinkSync("day5/tha5.txt");
-
-fs.rmdirSync("day5");
